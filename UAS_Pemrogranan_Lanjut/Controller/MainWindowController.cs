319b10c92d23a774657e93247afe728014373ceb
nibrasanersa <61958413+nibrasanersa@users.noreply.github.com>
2021-01-12 16:01:55 +0700

    Add files via upload

---
+++ UAS_Pemrogranan_Lanjut/Controller/MainWindowController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UAS.Model;
+
+namespace UAS.Controller
+{
+    
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja, Pembayaran pembayaran)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+
+        public void removeItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+
+        public List<Item> getItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void addDiskon(Diskon diskon)
+        {
+            
+            this.keranjangBelanja.addDiskon(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.keranjangBelanja.getDiskon();
+        }
+
+
+    }
+}
---
+++ UAS_Pemrogranan_Lanjut/Readme.md
@@ -0,0 +1,15 @@
+﻿# # Kedai Coffe 
+(Introduction)
+Aplikasi ini merupakan sebuah program Kedai Coffe untuk memenuhi tugas UAS Pemrograman Lanjut 2020/2021.
+## Scope of functionalities
+-   User bisa menekan tombol  `Daftar Menu`, `Ganti Promo`, dan `Klik 2x item di keranjang untuk menghapus item`
+-   User hanya bisa menghapus item (`Klik 2x`) ketika keranjang belanja  terisi.
+## How does it works?
+-   Terdapat folder Model yang berisikan class  `Diskon.cs`, `Item.cs`, `KeranjangBelanja.cs`, `Pembayaran.cs`
+-   Terdapat`MainWindow.xaml`untuk menampilkan program 1 (tampilan depan)
+-   Terdapat`Menu.xaml`untuk menampilkan program 2 (daftar menu)
+-   Terdapat`Promo.xaml`untuk menampilkan program 3 (pilihan promo)
+-   Terdapat folder Controller yang berisi class  `MainWindowController.cs`  `MenuController.cs`  dan  `PromoController.cs`
+-   Terdapat class `MainWindow.xaml.cs`, `Menu.xaml.cs`, dan `Promo.xaml.cs`  untuk function
+
+NIBRAS F ANERSA 19.11.2853
